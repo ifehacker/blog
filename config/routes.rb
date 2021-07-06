ca31366ac9c45cd3ab3551b8c3e71ab4959d97a6
ifehacker <ife.oshoba@gmail.com>
2021-07-06 14:53:24 +0100

    VC, Authenntication with sorcery, Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
+  resources :authors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  
  root "articles#index"
  
   resources :articles do
-      resouces :comments
+
+      resources :comments
+
   end
+resources :authors
+
+resources  :author_sessions, only: [ :new, :create, :destroy ]
+
+get "login", to: "author_sessions#new"
+get "logout", to: "author_sessions#destroy"
+
 end
